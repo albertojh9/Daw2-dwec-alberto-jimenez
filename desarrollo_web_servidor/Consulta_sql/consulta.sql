4b67ce44cdb5bd425594cbaf5ab932e2ae5ee8aa
albertojh9 <ajimenezh11@educarex.es>
2025-09-17 15:57:39 +0200

    .

--- desarrollo_web_servidor/Consulta_sql/consulta.sql
+++
@@ -1,7 +0,0 @@
-SELECT p.nombre
-FROM PROFESORES p
-JOIN IMPARTE i ON p.cod_prof = i.cod_prof
-JOIN CENTROS c ON i.id_centro = c.id_centro
-WHERE p.especialidad = 'Informática'
-  AND i.turno = 'Tarde'
-  AND c.tipo = 'Público';
