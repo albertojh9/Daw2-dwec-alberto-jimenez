41d13d7972dc4709a1dfdba248468f6c78257317
albertojh9 <ajimenezh11@educarex.es>
2025-11-12 16:42:36 +0100

    .

---
+++ retos/MostrarPares.mjs
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------
+// Inicialización
+//-----------------------------------------------------------------------
+
+$("body").ready(() => {
+    $("#botonMostrarPares").on("click", mostrarPares);
+    $("#botonLimpiar").on("click", () => $('#textoResultado').val(''));
+});
+
+
+//-----------------------------------------------------------------------
+// Gestores de eventos
+//-----------------------------------------------------------------------
+
+function mostrarPares() {
+
+    $("#textoResultado").val('');
+    const numero = $("#inputNumero").val();
+
+    for(let n = 1;n <= numero;n++) {
+        if(n % 2 == 0) {
+            $("#textoResultado").val($("#textoResultado").val()+','+n);
+        }
+    }
+
+}
